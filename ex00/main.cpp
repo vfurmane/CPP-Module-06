f81396f78d20f592e43506ea5c7aeca798eefb78
vfurmane <vfurmane@student.42.fr>
2022-01-04 10:44:11 +0100

    feat: convert numbers and chars

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,11 +6,12 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 10:09:55 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/01/03 10:30:37 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/01/04 10:43:24 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdlib>
 
 void	print_usage_message(void)
 {
@@ -20,6 +21,52 @@ void	print_usage_message(void)
 	std::cout << "\tconvert <literal>" << std::endl;
 }
 
+void	identify_type(const std::string &literal)
+{
+	if ((literal[0] >= '0' && literal[0] <= '9')
+			|| (literal[0] == '-' && literal[1] >= '0' && literal[1] <= '9'))
+	{
+		if (literal.find(".") == std::string::npos)
+		{
+			int	nbr = atoi(literal.c_str());
+			std::cout << "char: " << static_cast<char>(nbr) << std::endl;
+			std::cout << "int: " << nbr << std::endl;
+			std::cout << "float: " << static_cast<float>(nbr) << ".0f" << std::endl;
+			std::cout << "double: " << static_cast<double>(nbr) << ".0" << std::endl;
+		}
+		else
+		{
+			double	nbr = atof(literal.c_str());
+			if (literal[literal.length() - 1] == 'f')
+			{
+				std::cout << "char: " << static_cast<char>(nbr) << std::endl;
+				std::cout << "int: " << static_cast<int>(nbr) << std::endl;
+				std::cout << "float: " << static_cast<float>(nbr) << (static_cast<int>(nbr) == static_cast<float>(nbr) ? ".0" : "") << "f" << std::endl;
+				std::cout << "double: " << static_cast<double>(nbr) << (static_cast<int>(nbr) == static_cast<double>(nbr) ? ".0" : "") << std::endl;
+			}
+			else
+			{
+				std::cout << "char: " << static_cast<char>(nbr) << std::endl;
+				std::cout << "int: " << static_cast<int>(nbr) << std::endl;
+				std::cout << "float: " << static_cast<float>(nbr) << (static_cast<int>(nbr) == static_cast<float>(nbr) ? ".0" : "") << "f" << std::endl;
+				std::cout << "double: " << static_cast<double>(nbr) << (static_cast<int>(nbr) == static_cast<double>(nbr) ? ".0" : "") << std::endl;
+			}
+		}
+	}
+	else if (literal.length() == 1)
+	{
+		char	nbr = literal[0];
+		std::cout << "char: " << nbr << std::endl;
+		std::cout << "int: " << static_cast<int>(nbr) << std::endl;
+		std::cout << "float: " << static_cast<float>(nbr) << ".0f" << std::endl;
+		std::cout << "double: " << static_cast<double>(nbr) << ".0" << std::endl;
+	}
+	else
+	{
+		std::cerr << "Impossible conversion..." << std::endl;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -27,7 +74,6 @@ int	main(int argc, char **argv)
 		print_usage_message();
 		return 1;
 	}
-	const std::string	literal = argv[1];
-	(void)literal; /* ===== DELETE ===== */
+	identify_type(argv[1]);
 	return 0;
 }
